522657ecc9f0474cc20a51ec85f0ef615fd4d6fe
SoumenBaral <shumen.sb@gmail.com>
2023-11-11 08:08:04 +0600

    make a mathod of entry show '

--- Cinema_Hall_managementApp.py
+++ Cinema_Hall_managementApp.py
@@ -5,11 +5,21 @@ class Star_Cinema:
     def entry_hall(self,hall):
         self.hall_list.append(hall)
 
-class Hall:
+class Hall(Star_Cinema):
     def __init__(self,row,cal,hallNo) -> None:
         self.row       = row
         self.cal       = cal 
         self.hallNo    = hallNo
         self.seats     = []
         self.show_List = []
+
+        self.my_hall = row,cal,hallNo
+        self.entry_hall(self.my_hall)
     
+    def entry_show(self,id,movie_name,time):
+        self.id = id 
+        self.movie_name = movie_name
+        self.time = time 
+
+        movie_info = (id,movie_name,time)
+        self.show_List.append(movie_info)
